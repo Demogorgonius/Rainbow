f06d5db9e9d2c4bff568a7d0caed04cff87cdf21
Demogorgonius <51359795+Demogorgonius@users.noreply.github.com>
2023-11-13 15:12:47 +0300

    Starting project
    Starting Project

--- Rainbow/Services/CustomView.swift
+++ Rainbow/Services/CustomView.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+
+class CustomView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setViews()
+        layoutViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setViews()
+        layoutViews()
+    }
+    
+    func setViews() {
+        backgroundColor = .white
+    }
+    
+    func layoutViews() {
+        
+    }
+}
