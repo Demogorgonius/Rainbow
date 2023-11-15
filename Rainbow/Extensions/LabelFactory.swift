1e429ef749cfed3657bb231b6010355bec7b5cdd
DmKeller <dmit.keller@gmail.com>
2023-11-15 10:48:08 +0300

    fix bugs LabelFactory

--- Rainbow/Extensions/LabelFactory.swift
+++ Rainbow/Extensions/LabelFactory.swift
@@ -6,33 +6,54 @@
 //
 
 import UIKit
-
-import UIKit
-
+// Протокол для настраиваемого label
+protocol LabelConfigurable {
+    var textColor: UIColor { get }
+    var font: UIFont { get }
+}
+// Виды настраиваемых лейблов
 enum LabelType {
     case gamePink, gameBlack, gameLightGreen
 }
-
+// Фабрика для создания настраиваемых лейблов
 class LabelFactory {
     
-    static func createLabel(type: LabelType) -> UILabel {
+    static func createLabel(type: LabelType, text: String) -> UILabel {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.TextFont.Regular.size(of: 20)
-        
-        switch type {
-        case .gamePink:
-            label.text = "игра №1"
-            label.textColor = UIColor.RainbowGameColor.customPink
-        case .gameBlack:
-            label.text = "время: 11.0"
-            label.textColor = UIColor.RainbowGameColor.customBlack
-        case .gameLightGreen:
-            label.text = "угадано 1/1"
-            label.textColor = UIColor.RainbowGameColor.customLightGreen
-            label.font = UIFont.DisplayFont.Heavy.size(of: 20)
-        }
+        label.text = text
 
+        if let config = labelConfiguration(for: type) {
+            label.textColor = config.textColor
+            label.font = config.font
+        }
+        
         return label
     }
+    // Возвращает конфигурацию для заданного типа лейбла
+    private static func labelConfiguration(for type: LabelType) -> LabelConfigurable? {
+        switch type {
+        case .gamePink:
+            return GamePinkLabelConfig()
+        case .gameBlack:
+            return GameBlackLabelConfig()
+        case .gameLightGreen:
+            return GameLightGreenLabelConfig()
+        }
+    }
+}
+// Структуры для конфигураций различных видов лейблов
+struct GamePinkLabelConfig: LabelConfigurable {
+    let textColor: UIColor = UIColor.RainbowGameColor.customPink
+    let font: UIFont = UIFont.TextFont.Regular.size(of: 20)
+}
+
+struct GameBlackLabelConfig: LabelConfigurable {
+    let textColor: UIColor = UIColor.RainbowGameColor.customBlack
+    let font: UIFont = UIFont.TextFont.Regular.size(of: 20)
+}
+
+struct GameLightGreenLabelConfig: LabelConfigurable {
+    let textColor: UIColor = UIColor.RainbowGameColor.customLightGreen
+    let font: UIFont = UIFont.DisplayFont.Heavy.size(of: 20)
 }
