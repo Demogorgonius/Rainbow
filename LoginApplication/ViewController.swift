46f70e38583d54e5bc5e29a25c330beeb52c4077
Danila Bolshakov <danila1999777@gmail.com>
2023-11-22 14:26:03 +0300

    Initial commit
    Initial settings

---
+++ LoginApplication/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  LoginApplication
+//
+//  Created by Danila Bolshakov on 22.11.2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
